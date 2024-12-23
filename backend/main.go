c2651a1472ce92a520665bcff819293362dfc5c5
João Victor <joao_victor-321@hotmail.com>
2024-12-22 22:31:08 -0300

    update

--- backend/main.go
+++ backend/main.go
@@ -4,18 +4,21 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/joaovictor01/go-realtime-chat/backend/pkg/websocket"
 )
 
 func serveWs(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(r.Host)
 
 	// upgrade this connection to a WebSocket connection
-	ws, err := upgrader.Upgrade(w, r, nil)
+	ws, err := websocket.Upgrade(w, r, nil)
 	if err != nil {
-		log.Println(err)
+		log.Fprintf(w, "%+V\n", err)
 	}
+	go websocket.Writer(ws)
+	websocket.Reader(ws)
 
-	reader(ws)
 }
 
 func setupRoutes() {
